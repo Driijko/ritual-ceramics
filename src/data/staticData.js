4107ad5ee5c9832edff980a1c0f0605b4854d6d4
Driijko <driijko@gmail.com>
2023-06-22 18:24:52 -0400

    staticData.js

---
+++ src/data/staticData.js
@@ -0,0 +1,14 @@
+// AR = aspect-ratio
+export const layout = {
+  portraitARWidth: 9,
+  portraitARHeight: 16,
+  landscapeARWidth: 16,
+  landscapeARHeight: 9,
+  maxLandscapeWidth: 1600,
+  maxLandscapeHeight: 900,
+  minInterfaceAreaWidth: 400,
+  maxInterfaceAreaWidth: 600,
+  interfaceAreaPadding: 40
+}
+
+export const startingPageName = "splash";
