ae7fceae1481daf6e8bd444b50839338b237da78
Driijko <driijko@gmail.com>
2023-06-29 15:23:22 -0400

    SiteMenu: background audio tab desktop version.

--- src/components/1-site/interface/site-menu/SiteMenu.svelte
+++ src/components/1-site/interface/site-menu/SiteMenu.svelte
@@ -223,36 +223,40 @@ h1 {
   align-items: center;
 }
 .site-menu :global(.site-settings input[type="checkbox"]) {
-  transition-property: border-radius;
+  transition-property: border-radius, background-color;
   transition-duration: 0.5s;
   transition-timing-function: ease-out;
   border-style: solid;
-  border-color: black;
+  border-color: hsl(var(--hue2), 100%, 10%);
+}
+.site-menu :global(.site-settings input[type="checkbox"]:checked) {
+  border-radius: 50% !important;
+  background-color: hsl(var(--hue1), 100%, 40%) !important;
 }
 .site-menu :global(input[type="range"]::-webkit-slider-runnable-track) {
-  background-color: transparent;
+  background-color: hsl(var(--hue1), 100%, 60%);
   border-style: solid;
-  border-color: black;
+  border-color: hsl(var(--hue2), 100%, 10%);
   height: 100%;
   border-radius: 50px;
 }
 .site-menu :global(input[type="range"]::-moz-range-track) { 
-  background-color: transparent;
+  background-color: hsl(var(--hue1), 100%, 60%);
   border-style:solid;
-  border-color: black;
+  border-color: hsl(var(--hue2), 100%, 10%);
   border-width: 5px;
   height: 80%;
 }
 .site-menu :global(input[type="range"]::-webkit-slider-thumb) {
-  background-color: grey; 
+  background-color: hsl(var(--hue1), 100%, 40%);
   border-style: solid;
-  border-color: black;
+  border-color: hsl(var(--hue2), 100%, 10%);
   border-radius: 50%;
 }
 .site-menu.portrait :global(input[type="range"]::-moz-range-thumb) {
-  background-color: grey;
+  background-color: hsl(var(--hue1), 100%, 40%);
   border-style: solid;
-  border-color: black;
+  border-color: hsl(var(--hue2), 100%, 10%);
   border-radius: 50%;
 }
 /* SITE SETTINGS: PORTRAIT ----------------------------- */
@@ -277,12 +281,7 @@ h1 {
   height: calc(var(--iw)/7);
   border-width: calc(var(--iw)/80);
   border-radius: calc(var(--iw)/100);
-  border-color: hsl(var(--hue2), 100%, 10%);
   background-color: hsl(var(--hue1), 100%, 60%);
-}
-.site-menu.portrait :global(.site-settings input[type="checkbox"]:checked) {
-  border-radius: 50%;
-  background-color: hsl(var(--hue1), 100%, 50%);
 }
 .site-menu.portrait :global(.site-settings .volume-adjust) {
   flex-direction: column;
@@ -294,31 +293,30 @@ h1 {
 }
 .site-menu.portrait :global(input[type="range"]::-webkit-slider-runnable-track) {
   border-width: calc(var(--iw)/100);
-  border-color: hsl(var(--hue2), 100%, 10%);
-  background-color: hsl(var(--hue1), 100%, 60%);
 }
 .site-menu.portrait :global(input[type="range"]::-moz-range-track) { 
   border-radius: 50px;
-  border-color: hsl(var(--hue2), 100%, 10%);
 }
 .site-menu.portrait :global(input[type="range"]::-webkit-slider-thumb) {
   border-width: calc(var(--iw)/60);
   width: calc(var(--iw)/10);
   height: calc(var(--iw)/10);
   margin-top: calc(var(--iw)/-31); 
-  border-color: hsl(var(--hue2), 100%, 10%);
-  background-color: hsl(var(--hue1), 100%, 50%);
 }
 .site-menu.portrait :global(input[type="range"]::-moz-range-thumb) {
   border-width: 5px;
   width: 15%;
   height: 40%;
   margin-top: -4.8%; 
-  border-color: hsl(var(--hue2), 100%, 10%);
 }
 /* SITE SETTINGS: LANDSCAPE -------------------- */
 .site-menu.landscape :global(.site-settings) {
   gap: calc(var(--iw)/10);
+  background-image: linear-gradient(
+    90deg,
+    hsl(var(--hue2), 100%, 70%),
+    hsl(var(--hue1), 100%, 70%)
+  );
 }
 .site-menu.landscape :global(.site-settings label) {
   font-size: calc(var(--iw)/50);
@@ -332,9 +330,6 @@ h1 {
   height: calc(var(--iw)/30);
   border-width: calc(var(--iw)/300);
   border-radius: calc(var(--iw)/200);
-}
-.site-menu.landscape :global(.site-settings input[type="checkbox"]:checked) {
-  border-radius: 50%;
 }
 .site-menu.landscape :global(input[type="range"]) {
   width: calc(var(--iw)/4);
